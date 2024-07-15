782c3d1a4d5df5baf26ae08300b5de42b817d46d
Adam <adamplesca1:gmail.com>
2024-07-15 14:56:39 +0100

    fully complete logic

--- gameFunctions.java
+++ gameFunctions.java
@@ -1,15 +1,11 @@
 public class gameFunctions {
-    
-    private int playerWon, botWon ,tie;
+
     private int totalPlayerWins, totalBotWins, totalTies;
     private String playerAnswer, result;
 
+    botPlayer bot = new botPlayer();
 
     public gameFunctions(){
-        playerWon = 0;
-        botWon = 0;
-        tie = 0;
-
         totalPlayerWins = 0;
         totalBotWins = 0;
         totalTies = 0;
@@ -19,21 +15,43 @@ public class gameFunctions {
     }
 
 
-    //set + get player ans
+    //set + get player ans 
     public void setAnswer(String playerAnswer){
-        this.playerAnswer = playerAnswer;
+        this.playerAnswer = playerAnswer.toLowerCase();
     }
     public String getAnswer(){
         return playerAnswer;
     }
 
-    //calculates who won  //TODO calculate who won, give answer + track W/L/T
+    //calculates who won
     public void winCalc(){
-        if(playerAnswer.equals(bot.getBotMove()) )
+        String botMove = bot.getBotMove();
+
+        if(playerAnswer.equals(botMove)){
+            totalTies += 1;
+            result = "Tie";
+        } else if(playerAnswer.equals("rock") && botMove.equals("scissors") ||
+                  playerAnswer.equals("paper") && botMove.equals("rock") ||
+                  playerAnswer.equals("scissors") && botMove.equals("paper")){
+            totalPlayerWins += 1;
+            result = "Player wins";
+        } else {
+            totalBotWins += 1;
+            result = "Bot wins";
+        }
     }
 
-    public int returnWinner(){
-        return 0;
+    //returns results/info to user
+    public String returnWinner(){
+        return result;
     }
-
+    public int returnTotalTies(){
+        return totalTies;
+    }
+    public int returnPlayerWins(){
+        return totalPlayerWins;
+    }
+    public int returnBotWins(){
+        return totalBotWins;
+    }
 }
